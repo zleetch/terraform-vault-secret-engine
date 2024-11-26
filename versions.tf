66fd41699bd1fb403b3ab1e89a05df71350c1c6c
zleetch <cyber.scream99@gmail.com>
2024-11-26 11:39:32 +0700

    Init commit

--- versions.tf
+++ versions.tf
@@ -1,12 +1,10 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     vault = {
       source  = "hashicorp/vault"
-      version = "4.5.0"
+      version = ">= 4.5.0"
     }
   }
-}
-
-provider "vault" {
-
 }
